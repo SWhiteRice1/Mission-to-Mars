3a0425d00b6474d05ac1ddcaa7f9ec3bec5c7b0f
Skylar <skylar.white139@gmail.com>
2022-09-02 22:31:54 -0600

    Final Mars data scrape.

---
+++ Mission_to_Mars_Challenge.py
@@ -0,0 +1,272 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from splinter import Browser\n",
+    "from bs4 import BeautifulSoup as soup\n",
+    "from webdriver_manager.chrome import ChromeDriverManager\n",
+    "from selenium import webdriver \n",
+    "from selenium.webdriver.chrome.options import Options\n",
+    "from selenium.webdriver.support import expected_conditions as EC\n",
+    "import pandas as pd\n",
+    "import ssl\n",
+    "# import time"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "executable_path = {'executable_path': ChromeDriverManager().install()}\n",
+    "browser = Browser('chrome', **executable_path, headless=False)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Visit the mars nasa news site\n",
+    "url = 'https://redplanetscience.com'\n",
+    "browser.visit(url)\n",
+    "# Optional delay for loading the page\n",
+    "browser.is_element_present_by_css('div.list_text', wait_time=1)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "html = browser.html\n",
+    "news_soup = soup(html, 'html.parser')\n",
+    "slide_elem = news_soup.select_one('div.list_text')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Select the title from the articles\n",
+    "slide_elem.find('div', class_='content_title')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Use the parent element to find the first `a` tag and save it as `news_title`\n",
+    "news_title = slide_elem.find('div', class_='content_title').get_text()\n",
+    "news_title"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Use the parent element to find the paragraph text\n",
+    "news_p = slide_elem.find('div', class_='article_teaser_body').get_text()\n",
+    "news_p"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "### Featured Images"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Visit URL\n",
+    "url = 'https://spaceimages-mars.com'\n",
+    "browser.visit(url)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Find and click the full image button\n",
+    "full_image_elem = browser.find_by_tag('button')[1]\n",
+    "full_image_elem.click()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Parse the resulting html with soup\n",
+    "html = browser.html\n",
+    "img_soup = soup(html, 'html.parser')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Find the relative image url\n",
+    "img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')\n",
+    "img_url_rel"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Use the base URL to create an absolute URL\n",
+    "img_url = f'https://spaceimages-mars.com/{img_url_rel}'\n",
+    "img_url"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "df = pd.read_html('https://galaxyfacts-mars.com')[0]\n",
+    "df.columns=['description', 'Mars', 'Earth']\n",
+    "df.set_index('description', inplace=True)\n",
+    "df"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Change the dataframe to html\n",
+    "df.to_html()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "Hemispheres"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 1. Use browser to visit the URL \n",
+    "url = 'https://marshemispheres.com/'\n",
+    "\n",
+    "browser.visit(url)\n",
+    "\n",
+    "html = browser.html\n",
+    "img_soup = soup(html, 'html.parser')\n",
+    "img_soup"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 2. Create a list to hold the images and titles.\n",
+    "hemisphere_image_urls = []\n",
+    "\n",
+    "# 3. Write code to retrieve the image urls and titles for each hemisphere.\n",
+    "for i in range(4):\n",
+    "    #create empty dictionary\n",
+    "    hemispheres = {}\n",
+    "    browser.find_by_css('a.product-item h3')[i].click()\n",
+    "    img_url_rel = img_soup.find('img', class_='thumb').get('src')\n",
+    "    img_url = f'https://spaceimages-mars.com/{img_url_rel}'\n",
+    "    title = browser.find_by_css(\"h2.title\").text\n",
+    "    hemispheres[\"img_url\"] = img_url\n",
+    "    hemispheres[\"title\"] = title\n",
+    "    hemisphere_image_urls.append(hemispheres)\n",
+    "    browser.back()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 4. Print the list that holds the dictionary of each image url and title.\n",
+    "hemisphere_image_urls"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 5. Quit the browser\n",
+    "browser.quit()"
+   ]
+  }
+ ],
+ "metadata": {
+  "interpreter": {
+   "hash": "afbbcf307b2af83beec3fe6e1d52e74b8e1ec96bc45a64f10134ecf9ea2bb093"
+  },
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
